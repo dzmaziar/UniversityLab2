4de9eb5b81fa8af302b61e4c67fa18d395392de7
Dima Maziar <dzmaziar@mail.ru>
2021-10-10 15:59:13 +0300

    CREATE MainFrame class with Fields and const

---
+++ src/MainFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;
+
+
+    private JTextField resultField;
+    private JTextField dxField;
+    private JTextField dyField;
+
+    private ButtonGroup radio = new ButtonGroup();
+    private Box boxFormulaType = Box.createHorizontalBox();
+
+
+}
