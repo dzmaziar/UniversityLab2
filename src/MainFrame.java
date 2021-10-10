14a840ce98c0538916f23c3e4e03d7d38cb3188b
Dima Maziar <dzmaziar@mail.ru>
2021-10-10 16:06:19 +0300

    ADDED function1 and function2

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.ComponentListener;
 
 public class MainFrame extends JFrame {
     private static final int WIDTH = 400;
@@ -8,9 +9,20 @@ public class MainFrame extends JFrame {
     private JTextField resultField;
     private JTextField dxField;
     private JTextField dyField;
+    private JTextField dzField;
 
     private ButtonGroup radio = new ButtonGroup();
     private Box boxFormulaType = Box.createHorizontalBox();
 
 
+
+    public double function1(double x, double y, double z)
+    {
+        return (Math.pow(Math.cos(Math.exp(x)) + Math.pow(Math.log(1 + y), 2) + Math.sqrt(Math.exp(Math.cos(x)) + Math.sin(Math.PI * z) * Math.sin(Math.PI * z)) + Math.sqrt(1 / x) + Math.cos(y * y),
+                Math.sin(z)));
+    }
+    public double function2(double x,double y,double z)
+    {
+        return ((1+Math.sqrt(z*x))/(Math.pow(1+x*x*x,1/y)));
+    }
 }
